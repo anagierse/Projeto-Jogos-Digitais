805e5c5819c7398b0f40d7533b1c6fb3ff6d76fe
anagierse <anagierseray@gmail.com>
2025-04-19 18:30:58 -0300

    Tela descendo

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import pygame
 from cenario.rua import Rua
 
-pygame.init()
-
-tela_largura, tela_altura = 800, 600
-display = pygame.display.set_mode((tela_largura, tela_altura))
-cor_fundo = (165, 219, 142)
-
-rua = Rua(tela_largura)
-fps = pygame.time.Clock()
-
-loop = True
-while loop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            loop = False  
-            pygame.quit()  
-            quit()  
+def main():
+    pygame.init()
     
-    display.fill(cor_fundo)
-    rua.desenhar(display)
+    tela_largura, tela_altura = 800, 600
+    display = pygame.display.set_mode((tela_largura, tela_altura))
     
-    pygame.display.flip()
-    fps.tick(60)
+    cor_fundo = (165, 219, 142)
+    rua = Rua(tela_largura, tela_altura)
+    clock = pygame.time.Clock()
+    
+    velocidade = 3  
+    running = True
+    
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        rua.atualizar(velocidade)
+        
+        display.fill(cor_fundo)
+        rua.desenhar(display)
+        pygame.display.flip()
+        
+        clock.tick(60) 
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
