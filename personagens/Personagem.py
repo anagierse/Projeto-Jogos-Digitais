735245972dcd6c3a99a1377a3ec4b4a3d711c82b
Erica <159991570+Am-erica@users.noreply.github.com>
2025-05-07 00:01:53 -0300

    Update Personagens.py
    personagem criança feito, vilão feito, personagem adolescente falta terminar

--- personagens/Personagem.py
+++ personagens/Personagem.py
@@ -1,24 +1,95 @@
 import pygame
 import sys
 
-class Personagem1(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+class PersonagemBase(pygame.sprite.Sprite):
+    def __init__(self, imagem_path, x, y, teclas_controle, tamanho):
         super().__init__()
-        self.image = pygame.image.load('personagens/imagens/personagemCrianca.png')
-        self.image = pygame.transform.scale(self.image, (150, 150))
+        self.image = pygame.image.load(imagem_path)
+        self.image = pygame.transform.scale(self.image, tamanho)
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         self.velocidade = 5
+        self.teclas = teclas_controle
 
-    def update(self, teclas):
-        if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
-            self.rect.x -= self.velocidade
-        if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
+    def update(self, teclas_pressionadas):
+        for direcao, teclas in self.teclas.items():
+            if any(teclas_pressionadas[tecla] for tecla in teclas):  
+                if direcao == "esquerda":
+                    self.rect.x -= self.velocidade
+                elif direcao == "direita":
+                    self.rect.x += self.velocidade
+                elif direcao == "cima":
+                    self.rect.y -= self.velocidade
+                elif direcao == "baixo":
+                    self.rect.y += self.velocidade
+
+        # Limites da tela
+        if self.rect.left < -30:
+            self.rect.left = -30
+        if self.rect.right > 830:
+            self.rect.right = 830
+        if self.rect.top < -10:
+            self.rect.top = -10
+        if self.rect.bottom > 610:
+            self.rect.bottom = 610
+
+        if self.rect.bottom > 600:
+            pygame.quit()
+            sys.exit()
+
+    def desenhar(self, display):
+        display.blit(self.image, self.rect.topleft)
+
+class Personagem1(PersonagemBase):
+    def __init__(self, x, y):
+        teclas_controle = {
+            "esquerda": [pygame.K_a, pygame.K_LEFT],
+            "direita": [pygame.K_d, pygame.K_RIGHT],
+            "cima": [pygame.K_w, pygame.K_UP],
+            "baixo": [pygame.K_s, pygame.K_DOWN]
+        }
+        tamanho = (100, 120)
+        super().__init__('personagens/imagens/personagemCrianca.png', x, y, teclas_controle, tamanho)
+
+class Personagem2(PersonagemBase):
+    def __init__(self, x, y):
+        teclas_controle = {
+            "esquerda": [pygame.K_a, pygame.K_LEFT],
+            "direita": [pygame.K_d, pygame.K_RIGHT],
+            "cima": [pygame.K_w, pygame.K_UP],
+            "baixo": [pygame.K_s, pygame.K_DOWN]
+        }
+        tamanho = (150, 150)
+        super().__init__('personagens/imagens/personagemAdolescente.png', x, y, teclas_controle, tamanho)
+
+class Vilao(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.image.load('personagens/imagens/homemdosaco.png')
+        self.image = pygame.transform.scale(self.image, (120, 150))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.velocidade = 1#depois mudar velocidade
+
+    def update(self, personagem):
+        if personagem.rect.centerx > self.rect.centerx:
             self.rect.x += self.velocidade
-        if teclas[pygame.K_UP] or teclas[pygame.K_w]:
-            self.rect.y -= self.velocidade
-        if teclas[pygame.K_DOWN] or teclas[pygame.K_s]:
+        elif personagem.rect.centerx < self.rect.centerx:
+            self.rect.x -= self.velocidade
+
+        if personagem.rect.centery > self.rect.centery:
             self.rect.y += self.velocidade
+        elif personagem.rect.centery < self.rect.centery:
+            self.rect.y -= self.velocidade
+
+        if self.rect.left < -30:
+            self.rect.left = -30
+        if self.rect.right > 830:
+            self.rect.right = 830
+        if self.rect.top < -10:
+            self.rect.top = -10
+        if self.rect.bottom > 610:
+            self.rect.bottom = 610
 
     def desenhar(self, display):
         display.blit(self.image, self.rect.topleft)
