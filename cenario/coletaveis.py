e2607ccf9a9a6ef316144857c4b75acdc868fd45
ViktorAlves <162377466+ViktorAlves@users.noreply.github.com>
2025-05-15 08:34:06 -0300

    Update coletaveis.py
    caderno adcionado

--- cenario/coletaveis.py
+++ cenario/coletaveis.py
@@ -41,6 +41,24 @@ class Pilula:
     def desenhar(self, tela):
         tela.blit(self.imagem, (self.pos_X, self.pos_Y))
 
+class Caderno:
+    def __init__(self, x_pos, y_pos):
+        self.largura = 40
+        self.altura = 40
+        self.pos_X = x_pos
+        self.pos_Y = y_pos
+        self.velocidade = 2
+        self.imagem_original = pygame.image.load('cenario/imagens/caderno.png').convert_alpha()
+        self.imagem = pygame.transform.scale(self.imagem_original, (self.largura, self.altura))
+        self.rect = pygame.Rect(self.pos_X, self.pos_Y, self.largura, self.altura)
+
+    def atualizar(self):
+        self.pos_Y += self.velocidade
+        self.rect.y = self.pos_Y
+
+    def desenhar(self, tela):
+        tela.blit(self.imagem, (self.pos_X, self.pos_Y))
+
 
 
 class Dinheiro:
